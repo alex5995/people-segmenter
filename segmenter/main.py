ddcbed401d4a7e02351b247c6a093cc44b68e598
Alex Ceccotti <alex@localhost.localdomain>
2020-11-16 15:33:56 +0100

    update API for using the model

--- segmenter/main.py
+++ segmenter/main.py
@@ -1,11 +1,16 @@
+import sys
+sys.path.insert(0, 'segmenter')
+
 from fastapi import FastAPI, File, UploadFile, Response
 from PIL import Image
 from io import BytesIO
+from model import load_model, predict_mask
 
 app = FastAPI()
+model = load_model('people-segmenter-model/model.bin')
 
 def read_image_file(file) -> Image.Image:
-    img = Image.open(BytesIO(file)).convert("L").convert("RGB").resize((224,224))
+    img = Image.open(BytesIO(file)).convert("L").convert("RGB")
     return img
     
 def get_image_bytes(img):
@@ -16,5 +21,6 @@ def get_image_bytes(img):
 @app.post("/predict/image")
 async def predict_api(file: UploadFile = File(...)):
     img = read_image_file(await file.read())
-    return Response(get_image_bytes(img), media_type="image/png")
+    masked_img = predict_mask(model, img)
+    return Response(get_image_bytes(masked_img), media_type="image/png")
 
